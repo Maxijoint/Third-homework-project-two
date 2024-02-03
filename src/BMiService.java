890c895e59ceb7287d666011b9842e95c6557739
Maxijoint <maxijoint@yandex.ru>
2024-02-03 11:09:03 +0300

    Change

---
+++ src/BMiService.java
@@ -0,0 +1,9 @@
+public class BMiService {
+
+       public float calculate(float weight, float height){
+           float massIndex = weight / (height * height);
+           return massIndex;
+       }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        BMiService service = new BMiService();
+        float result = service.calculate(83f, 1.85f);
+        System.out.println("Ваш индекс тела составляет:" + result);
+    }
+}
