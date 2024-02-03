cb6d72d9603053ceda1cd17f07a3a636dd52f8cd
Maxijoint <maxijoint@yandex.ru>
2024-02-03 14:21:26 +0300

    Update

--- src/BMiService.java
+++ src/BMiService.java
@@ -1,9 +1,9 @@
 public class BMiService {
 
-       public float calculate(float weight, float height){
-           float massIndex = weight / (height * height);
-           return massIndex;
-       }
+    public float calculate(float weight, float height) {
+        float massIndex = weight / (height * height);
+        return massIndex;
+    }
 
 
 }
